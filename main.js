e681080a8a39280b84c01526aa2737d3357edc92
Geniorama Agencia <geniorama.info@gmail.com>
2023-02-28 06:04:25 -0500

    fix: ajuste plantilla 2

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const gallery = document.querySelector('.proyectos-plantilla-2 .gallery')
 
 if(gallery){
-    if(gallery.querySelectorAll('.gallery__item').length < 3){
+    if(gallery.querySelectorAll('.gallery__item').length == 2){
         gallery.classList.add('grid-2-columns')
     }
 }
